d3f0fdbbf4516d05cc950c8bfe365e1953bcfe38
lisahuele <lisahuele@gmail.com>
2021-02-24 19:53:46 -0500

    sticky updated

--- js/blm.js
+++ js/blm.js
@@ -17,3 +17,16 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var stickyTop = $(".navigation-section").offset().top;
+//whenever the user scrolls, measure how far we have scrolled
+$(window).scroll(function() {
+    var windowTop = $(window).scrollTop();
+  
+    if (windowTop > stickyTop) {
+  
+      $(".navigation-section").addClass("stuck");
+    } else {   
+      $(".navigation-section").removeClass("stuck");
+    }
+  });
