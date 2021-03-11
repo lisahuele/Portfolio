d0ea2f0ee5c4d4d5f60f00e0989c4489fa72b3a9
lisahuele <lisahuele@gmail.com>
2021-03-11 09:45:40 -0500

    updated

--- js/index.js
+++ js/index.js
@@ -42,3 +42,24 @@ function scrollAnchors(e, respond = null) {
         }
     }, 100);
 }
+
+$('.scrollHit').click(function(event){
+    
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+});
