7dd88f36bac1419b3cd70b8f4537a1e7b7ba78d8
lisahuele <lisahuele@gmail.com>
2021-03-25 18:20:29 -0400

    reorganized code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function(){
-    $(document).scroll(function(){
-        if($(this).scrollTop() >= $('#approach').offset().top - 100) {
+  $(document).scroll(function(){
+       if($(this).scrollTop() >= $('#approach').offset().top) {
             $(".header").addClass("header-stuck");
             $(".slice").addClass("slice-stuck");
             $(".top").addClass("top-stuck");
@@ -15,33 +15,6 @@ $(function(){
     });
   });
 
-  (function() {
-    scrollTo();
-})();
-
-function scrollTo() {
-    const links = document.querySelectorAll('.scrollWork');
-    links.forEach(each => (each.onclick = scrollAnchors));
-}
-
-function scrollAnchors(e, respond = null) {
-    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-    e.preventDefault();
-    var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-    const targetAnchor = document.querySelector(targetID);
-    if (!targetAnchor) return;
-    const originalTop = distanceToTop(targetAnchor);
-    window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-    const checkIfDone = setInterval(function() {
-        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-        if (distanceToTop(targetAnchor) === 0 || atBottom) {
-            targetAnchor.tabIndex = '-1';
-            targetAnchor.focus();
-            window.history.pushState('', '', targetID);
-            clearInterval(checkIfDone);
-        }
-    }, 100);
-}
 
 $('.scrollHit').click(function(event){
     
@@ -55,11 +28,11 @@ $('.scrollHit').click(function(event){
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
-          scrollTop: $(hash).offset().top
+          scrollTop: $(hash).offset().top -150
         }, 800, function(){
      
           // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
+         // window.location.hash = hash;
         });
       } // End if
 });
